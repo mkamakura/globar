b7f3c596b0ea4e98a9fa3b1cc7be81eb140f5231
mkamakura <kamakura.masaya@gmail.com>
2016-06-02 12:38:32 +0900

    fixed cli tests

--- tests/fixtures/mocha.js
+++ tests/fixtures/mocha.js
@@ -1,6 +1,6 @@
 // Mocha configuration
   beforeEach(function() {
     // Set the default timeouts for all tests
-    this.currentTest.timeout(2000);
+    this.currentTest.timeout(5000);
     this.currentTest.slow(100);
   });
--- tests/specs/cli.spec.js
+++ tests/specs/cli.spec.js
@@ -4,9 +4,18 @@ var childProcess = require('child_process');
 var assert = require('chai').assert;
 
 describe('cli', function() {
-  it('should handle error event for child-process then throw error own', function(done) {
-    childProcess.exec('../../bin/globify.js \"error_code/**/*.js\" -o \"dist/error_code/**/*.bundle.min.js\"', function (err) {
-      assert.isNotNull(err);
+  it('should return error code(non-zero) for globar then return non-zero', function(done) {
+    var ps = childProcess.exec('../../bin/globar \"error_code/**/*.js\" -o \"dist/error_code/**/*.bundle.min.js\"');
+    ps.on('exit', function(code) {
+      assert.isOk(code !== 0);
+      done();
+    });
+  });
+
+  it('should return successful code for globar then return zero', function(done) {
+    var ps = childProcess.exec('../../bin/globar \"lib/**/*.js\" -o \"dist/**/*.bundle.min.js\"');
+    ps.on('exit', function(code) {
+      assert.isOk(code === 0);
       done();
     });
   });
